e3b875fd5d4008381cd5013bb1a7d274a9504a1e
Soleil3214 <hayate2012as@yahoo.co.jp>
2018-05-09 01:26:12 +0900

    update05/09-3

---
+++ chap5-3-1.py
@@ -0,0 +1,11 @@
+#リストを使って数値を並べ替える練習
+
+list = [70, 15, 66, 21, 19, 97, 33, 44, 30, 2]
+for i in range(len(list)):
+    print(list[i])
+
+#range関数を使わない方法もある
+
+list = [70, 15, 66, 21, 19, 97, 33, 44, 30, 2]
+for i in list:
+    print(i)
---
+++ chap5-3-3.py
@@ -0,0 +1,15 @@
+#数字昇順並び替えプログラム
+
+list = [70, 15, 66, 21, 19, 97, 33, 44, 30, 2]
+disp = " "
+for k in range(len(list) - 1, 0, -1):
+    print(str(len(list) - k) + "度目")
+    for j in range(0, k):
+        if list[j] > list[j + 1]:
+            temp = list[j]
+            list[j] = list[j + 1]
+            list[j + 1] = temp
+    for i in list:
+        disp = disp + str(i) + " "
+        print(disp)
+        disp = " "
