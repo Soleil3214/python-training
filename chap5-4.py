53f9334ed6ef59bc1bf25911c8a2963721c90e92
Soleil3214 <hayate2012as@yahoo.co.jp>
2018-05-09 00:54:45 +0900

    update05/09-2

---
+++ chap5-4.py
@@ -0,0 +1,23 @@
+import tkinter
+from PIL import Image, ImageTk
+
+root = tkinter.Tk()
+root.title("王様をいっぱい表示")
+root.minsize(1600, 800)
+
+canvas = tkinter.Canvas(root, width=1600, height=800)
+canvas.pack()
+img = ImageTk.PhotoImage(Image.open('img4/chap4-1-1.png').convert('RGB'))
+
+#座標用変数
+x = 100
+y = 180
+
+while True:
+    canvas.create_image(x, y, image=img)
+    x = x + 200
+    y = y + 50
+    if x >= 1600 or y >= 480:
+        break
+    
+root.mainloop()
