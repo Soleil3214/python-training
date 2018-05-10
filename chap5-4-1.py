51e9df815531729e6f2f4485aadddf65be25d5c7
Soleil3214 <hayate2012as@yahoo.co.jp>
2018-05-10 23:58:45 +0900

    update05/10-1

---
+++ chap5-4-1.py
@@ -0,0 +1,25 @@
+import tkinter
+#ウィンドウの作成
+root = tkinter.Tk()
+root.title("棒グラフをソートして表示")
+#キャンバスの配置
+canvas = tkinter.Canvas(root, width=640, height=480)
+canvas.create_rectangle(40, 40, 600, 440, fill="gray78")
+canvas.pack()
+
+#リスト
+list = [70, 15, 66, 21, 19, 97, 33, 44, 30, 2]
+disp = ""
+#繰り返し処理
+for k in range(len(list) - 1, 0, -1):
+    print(str(10 - k) + "度目")
+    for j in range(0, k):
+        if list[j] > list[j + 1]:
+            temp = list[j]
+            list[j] = list[j + 1]
+            list[j + 1] = temp
+        for i in list:
+            disp = disp + str(i) + " "
+        print(disp)
+        disp = ""
+root.mainloop()
