a22ad274913d213dba5a88100a0787b3ae504cbb
Soleil3214 <hayate2012as@yahoo.co.jp>
2018-08-04 00:44:51 +0900

    08/04

--- chap6-3-1.py
+++ chap6-3-1.py
@@ -56,6 +56,13 @@ def ending():
     だが君の戦いはまだ始まったばかりだ。
     ・・・・つづく？""")
 
+    #invalidate button
+    button_up["state"] = "disabled"
+    button_down["state"] = "disabled"
+    button_left["state"] = "disabled"
+    button_right["state"] = "disabled"
+
+
 #window
 root = tkinter.Tk()
 root.title("ダンジョン")
